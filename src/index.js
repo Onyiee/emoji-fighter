3205da2f5038546d01e7f3add7034b5f80821129
Onyiee <elizabethonyemowo.agbo@gmail.com>
2025-03-31 05:15:31 +0100

    remove unused images

---
+++ src/index.js
@@ -0,0 +1,32 @@
+let fighters = [
+  "🐉",
+  "🐥",
+  "🐊",
+  "💩",
+  "🦍",
+  "🐢",
+  "🐩",
+  "🦭",
+  "🦀",
+  "🐝",
+  "🤖",
+  "🐘",
+  "🐸",
+  "🕷",
+  "🐆",
+  "🦕",
+  "🦁",
+];
+
+let stageEl = document.getElementById("stage");
+let fightButton = document.getElementById("fightButton");
+
+fightButton.addEventListener("click", function () {
+  // Challenge:
+  // When the user clicks on the "Pick Fighters" button, pick two random
+  // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+  let randomIndex1 = Math.floor(Math.random() * fighters.length);
+  let randomIndex2 = Math.floor(Math.random() * fighters.length);
+  return (stageEl.textContent =
+    fighters[randomIndex1] + " vs " + fighters[randomIndex2]);
+});
